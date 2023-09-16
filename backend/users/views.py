2c275ec984cd79d6fcac5c55c5557339a1ca4cd0
Nikolay Sinkov <nikolay.sinkov97@gmail.com>
2023-09-16 21:34:11 +0300

    Workflow

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,5 @@
+from api.paginator import CustomPaginator
+from api.serializers import ShowSubscriptionsSerializer, UserSerializer
 from django.db.models import Exists, OuterRef
 from django.shortcuts import get_object_or_404
 from djoser.views import UserViewSet
@@ -5,9 +7,6 @@ from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-
-from api.paginator import CustomPaginator
-from api.serializers import ShowSubscriptionsSerializer, UserSerializer
 from users.models import Follow, User
 
 
@@ -23,20 +22,31 @@ class UserViewSet(UserViewSet):
             is_subscribed=Exists(is_subscribed)
         )
 
-    @action(detail=True, methods=['POST', 'DELETE'])
+   @action(detail=True, methods=['POST', 'DELETE'])
     def subscribe(self, request, **kwargs):
+        """Method allows follow any user or unfollow."""
         user = request.user
         author_id = self.kwargs.get('id')
         author = get_object_or_404(User, id=author_id)
 
         if request.method == 'POST':
-            return self._subscribe_to_author(user, author, request.data)
+            serializer = ShowSubscriptionsSerializer(
+                author, data=request.data, context={'request': request}
+            )
+            serializer.is_valid(raise_exception=True)
+            Follow.objects.create(user=user, author=author)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
 
         if request.method == 'DELETE':
-            return self._unsubscribe_from_author(user, author)
+            subscription = get_object_or_404(
+                Follow, user=user, author=author
+            )
+            subscription.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
 
     @action(detail=False)
     def subscriptions(self, request):
+        """Method shows user's subscriptions."""
         user = request.user
         queryset = User.objects.filter(following__user=user)
         pages = self.paginate_queryset(queryset)
@@ -44,16 +54,3 @@ class UserViewSet(UserViewSet):
             pages, many=True, context={'request': request}
         )
         return self.get_paginated_response(serializer.data)
-
-    def _subscribe_to_author(self, user, author, data):
-        serializer = ShowSubscriptionsSerializer(
-            author, data=data, context={'request': self.request}
-        )
-        serializer.is_valid(raise_exception=True)
-        Follow.objects.create(user=user, author=author)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-    def _unsubscribe_from_author(self, user, author):
-        subscription = get_object_or_404(Follow, user=user, author=author)
-        subscription.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
