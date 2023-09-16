a9bbd496b88cc9e0a374209e34a23da8a28edd69
Nikolay Sinkov <nikolay.sinkov97@gmail.com>
2023-09-16 18:40:55 +0300

    Diplom, Review 1

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,7 @@
 from django.urls import include, path
-from rest_framework.routers import DefaultRouter
-
 from recipes.views import (FavoriteViewSet, IngredientViewSet, RecipeViewSet,
                            ShoppingCartViewSet, TagViewSet)
+from rest_framework.routers import DefaultRouter
 from users.views import UserViewSet
 
 app_name = 'api'
